5dc2765a4f63f3da1625a95d0b773c96183a5636
yongwoo117 <ywhss117@daum.net>
2022-10-04 19:10:42 +0900

    Refactoring + Dropdown implement

--- Assets/Scripts/StartManager.cs
+++ Assets/Scripts/StartManager.cs
@@ -1,38 +1,51 @@
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartManager : MonoBehaviour
 {
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    [SerializeField] private TMP_Dropdown screenModeDropdown;
     private AsyncOperation gameScene;
-    [SerializeField] private GameObject settingPanel;
-    [SerializeField] private List<GameObject> displaySettingUI;
-    [SerializeField] private List<GameObject> soundSettingUI;
+    private List<Resolution> resolutions;
 
-    private void SetUI(List<GameObject> uiList,bool enable)
-    {
-        foreach (var ui in uiList)
-            ui.SetActive(enable);
-    }
-    
     private void Start()
     {
         gameScene = SceneManager.LoadSceneAsync("Scenes/SampleScene");
         gameScene.allowSceneActivation = false;
-        settingPanel.SetActive(false);
+        SetupResolutionDropdown();
+        SetupScreenModeDropdown();
+    }
 
-        SetUI(displaySettingUI, false);
-        SetUI(soundSettingUI, false);
+    private void SetupResolutionDropdown()
+    {
+        resolutions = new List<Resolution>();
+        resolutions.AddRange(Screen.resolutions);
+        resolutionDropdown.options.Clear();
+        foreach (var resolution in resolutions)
+            resolutionDropdown.options.Add(new TMP_Dropdown.OptionData(resolution.ToString()));
+        resolutionDropdown.onValueChanged.AddListener(index =>
+            Screen.SetResolution(resolutions[index].width, resolutions[index].height, Screen.fullScreenMode));
+    }
+
+    private void SetupScreenModeDropdown()
+    {
+        var screenModeList = Enum.GetNames(typeof(FullScreenMode));
+        screenModeDropdown.options.Clear();
+        foreach (var screenMode in screenModeList)
+            screenModeDropdown.options.Add(new TMP_Dropdown.OptionData(screenMode));
+        screenModeDropdown.onValueChanged.AddListener(index =>
+        {
+            if(!Enum.TryParse(screenModeList[index], out FullScreenMode result)) return;
+            Screen.fullScreenMode = result;
+        });
     }
 
     public void OnStartButtonClick()
     {
         gameScene.allowSceneActivation = true;
-    }
-
-    public void OnSettingButtonClick()
-    {
-        settingPanel.SetActive(true);
     }
 
     public void OnGameExitButtonClick()
@@ -42,22 +55,5 @@ public class StartManager : MonoBehaviour
 #else
         Application.Quit();
 #endif
-    }
-
-    public void OnSettingExitButtonClick()
-    {
-        settingPanel.SetActive(false);
-    }
-
-    public void OnDisplaySettingButtonClick()
-    {
-        SetUI(soundSettingUI, false);
-        SetUI(displaySettingUI, true);
-    }
-
-    public void OnSoundSettingButtonClick()
-    {
-        SetUI(displaySettingUI,false);
-        SetUI(soundSettingUI, true);
     }
 }
