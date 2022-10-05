1f6489a1d01ec7a1706d8678c3938f42b2378cc7
yongwoo117 <ywhss117@daum.net>
2022-10-05 14:16:03 +0900

    Update RhythmInput.cs

--- Assets/Scripts/Player/RhythmInput.cs
+++ Assets/Scripts/Player/RhythmInput.cs
@@ -9,7 +9,19 @@ public class RhythmInput : MonoBehaviour
     //true일 때만 노트를 칠 수 있습니다.
     private bool rhythmFlag;
     
+    public UnityEvent<int> onRhythmStreakChanged;
     public UnityEvent<InteractionType, object> onInteraction;
+    
+    private int RhythmStreak
+    {
+        get => rhythmStreak;
+        set
+        {
+            rhythmStreak = value;
+            onRhythmStreakChanged.Invoke(rhythmStreak);
+        }
+    }
+    private int rhythmStreak;
     
     /// <summary>
     /// 너무 일찍 노트를 친 경우 호출됩니다.
@@ -24,6 +36,7 @@ public class RhythmInput : MonoBehaviour
     private void Start()
     {
         rhythmFlag = true;
+        rhythmStreak = 0;
     }
 
     public void OnInteraction(InteractionType type, object arg)
@@ -31,11 +44,13 @@ public class RhythmInput : MonoBehaviour
         if (rhythmFlag && RhythmCore.Instance.Judge())
         {
             onInteraction?.Invoke(type, arg);
+            RhythmStreak++;
         }
         else
         {
             onTooEarly?.Invoke();
             onInteraction?.Invoke(InteractionType.RhythmEarly, arg);
+            RhythmStreak = 0;
         }
         rhythmFlag = false;
     }
@@ -46,6 +61,7 @@ public class RhythmInput : MonoBehaviour
         {
             onTooLate?.Invoke();
             onInteraction?.Invoke(InteractionType.RhythmLate, null);
+            RhythmStreak = 0;
         }
 
         rhythmFlag = true;
