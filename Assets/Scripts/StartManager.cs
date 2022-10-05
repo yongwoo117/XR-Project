30c12cfd4c38a77987578387002a72c8a5ca388b
yongwoo117 <ywhss117@daum.net>
2022-10-05 13:11:36 +0900

    Update StartManager

--- Assets/Scripts/StartManager.cs
+++ Assets/Scripts/StartManager.cs
@@ -2,11 +2,15 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartManager : MonoBehaviour
 {
     [SerializeField] private TMP_Dropdown resolutionDropdown;
     [SerializeField] private TMP_Dropdown screenModeDropdown;
+    [SerializeField] private Slider masterVolumeSlider;
+    [SerializeField] private Slider bgmVolumeSlider;
+    [SerializeField] private Slider sfxVolumeSlider;
     private AsyncOperation gameScene;
 
     private void Start()
@@ -15,48 +19,81 @@ public class StartManager : MonoBehaviour
         gameScene.allowSceneActivation = false;
         SetupResolutionDropdown();
         SetupScreenModeDropdown();
+        SetupMasterVolumeSlider();
+        SetupBGMVolumeSlier();
+        SetupSFXVolumeSlider();
+    }
+
+    private void SetupDropdown(Array array, TMP_Dropdown dropdown, Action<int> onValueChanged, PrefsKey prefsKey,
+        int initialValue)
+    {
+        //드롭다운에 아이템 추가
+        dropdown.options.Clear();
+        foreach (var member in array)
+            dropdown.options.Add(new TMP_Dropdown.OptionData(member.ToString()));
+
+        //이벤트 등록
+        dropdown.onValueChanged.AddListener(index =>
+        {
+            Prefs.SetInt(prefsKey, index);
+            onValueChanged(index);
+        });
+        
+        //드롭다운 기본값 세팅
+        dropdown.value = Prefs.HasKey(prefsKey) ? Prefs.GetInt(prefsKey) : initialValue;
     }
 
     private void SetupResolutionDropdown()
     {
         var resolutions = Screen.resolutions;
-        
-        //드롭다운에 아이템 추가
-        resolutionDropdown.options.Clear();
-        foreach (var resolution in resolutions)
-            resolutionDropdown.options.Add(new TMP_Dropdown.OptionData(resolution.ToString()));
-        
-        //이벤트 등록
-        resolutionDropdown.onValueChanged.AddListener(index =>
-        {
-            PlayerPrefs.SetInt("ResolutionIndex", index);
-            Screen.SetResolution(resolutions[index].width, resolutions[index].height, Screen.fullScreenMode);
-        });
 
-        //드롭다운 기본값 세팅
-        resolutionDropdown.value = PlayerPrefs.HasKey("ResolutionIndex")
-            ? PlayerPrefs.GetInt("ResolutionIndex")
-            : resolutions.Length - 1;
+        SetupDropdown(resolutions, resolutionDropdown, index =>
+                Screen.SetResolution(resolutions[index].width, resolutions[index].height, Screen.fullScreenMode),
+            PrefsKey.ResolutionIndex, resolutions.Length - 1);
     }
 
     private void SetupScreenModeDropdown()
     {
         if (Enum.GetValues(typeof(FullScreenMode)) is not FullScreenMode[] screenModeList) return;
-        
-        //드롭다운에 아이템 추가
-        screenModeDropdown.options.Clear();
-        foreach (var screenMode in screenModeList)
-            screenModeDropdown.options.Add(new TMP_Dropdown.OptionData(screenMode.ToString()));
-        
+
+        SetupDropdown(screenModeList, screenModeDropdown, index =>
+            Screen.fullScreenMode = screenModeList[index], PrefsKey.ScreenModeIndex, 0);
+    }
+
+    private void SetupSlider(string path, Slider slider, PrefsKey prefsKey)
+    {
+        //버스 받아오기
+        // var bus = FMODUnity.RuntimeManager.GetBus(path);
+
         //이벤트 등록
-        screenModeDropdown.onValueChanged.AddListener(index =>
+        slider.onValueChanged.AddListener(value =>
         {
-            PlayerPrefs.SetInt("ScreenModeIndex", index);
-            Screen.fullScreenMode = screenModeList[index];
+            Prefs.SetFloat(prefsKey, value);
+            // bus.setVolume(value);
         });
-        
-        //드롭다운 기본값 세팅
-        screenModeDropdown.value = PlayerPrefs.HasKey("ScreenModeIndex") ? PlayerPrefs.GetInt("ScreenModeIndex") : 0;
+
+        //슬라이더 기본값 세팅
+        slider.value = Prefs.HasKey(prefsKey)
+            ? Prefs.GetFloat(prefsKey)
+            : slider.maxValue / 2;
+    }
+
+    private void SetupMasterVolumeSlider()
+    {
+        //TODO: path 수정 필요
+        SetupSlider("bus:/Master", masterVolumeSlider, PrefsKey.MasterVolume);
+    }
+
+    private void SetupBGMVolumeSlier()
+    {
+        //TODO: path 수정 필요
+        SetupSlider("bus:/BGM", bgmVolumeSlider, PrefsKey.BGMVolume);
+    }
+
+    private void SetupSFXVolumeSlider()
+    {
+        //TODO: path 수정 필요
+        SetupSlider("bus:/SFX", sfxVolumeSlider, PrefsKey.SFXVolume);
     }
 
     public void OnStartButtonClick()
--- Assets/Scripts/UIManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using UnityEngine;
-using UnityEngine.Events;
-
-public class UIManager : MonoBehaviour
-{
-    
-}
